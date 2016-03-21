326e5d535a4aa16f10b45b7f30a97ebcd3f3848a
Anton Babych <antogarudy@gmail.com>
2016-03-21 13:03:46 +0300

    hh1

---
+++ hh.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		;
+		int x;
+		x = 8;
+		int y;
+		y = 15;
+		double z;
+		z=x+=y;
+		System.out.println(z);
+		}	
+	}
+
+
