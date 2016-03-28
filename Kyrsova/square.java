7b42c78f45cb699315221c59032e4231f090f43a
Anton Babych <antogarudy@gmail.com>
2016-03-28 13:26:41 +0400

    kyrsova1

--- Kyrsova/square.java
+++ Kyrsova/square.java
@@ -6,9 +6,7 @@ public class square {
 		ss s1 = new ss ();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Ведіть радіус кола");
-        int r = scan.nextInt();
-        s1.r = r ;
-	 double Yourss = s1.calcsquare();
+       
 			System.out.println("Ваша площа кола " + Yourss);
 		}
 	}
